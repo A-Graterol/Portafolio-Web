bd006be40b456738612f26a632058e1fad412052
A-Graterol <andresaga47@gmail.com>
2024-08-17 22:39:52 -0400

    Actualizando portafolio completo

---
+++ index.js
@@ -0,0 +1,4 @@
+import './node_modules/eins-modal/dist/js/eins-modal.min.js'
+import './node_modules/eins-modal/dist/css/eins-modal.min.css'
+import './css/style.css'
+import './css/style-modal.css'
