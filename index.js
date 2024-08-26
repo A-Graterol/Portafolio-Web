0b8708ebf079cb886f975831653de32637553037
A-Graterol <andresaga47@gmail.com>
2024-08-26 18:47:11 -0400

    Verificando archivos y unificando

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 import './node_modules/eins-modal/dist/js/eins-modal.min.js'
 import './node_modules/eins-modal/dist/css/eins-modal.min.css'
 import './css/style.css'
-import './css/style-modal.css'
